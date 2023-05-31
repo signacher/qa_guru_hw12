ab9d0a75107b170f2bca59fef8fda258f1c5b668
Alexey Telnov <signacher@mail.ru>
2023-05-31 09:14:46 +0300

    fix choice envr

--- Test/conftest.py
+++ Test/conftest.py
@@ -3,9 +3,16 @@ from selene.support.shared import browser
 from selenium import webdriver
 from util import attach
 
-@pytest.fixture(scope="function",autouse = True )
-def setup_browser():
+def pytest_addoption(parser):
+    parser.addoption(
+        '--envr',
+        help='Среда, в которой будут запускатся тесты',
+        choises=['dev', 'stage', 'prod'],
+        default='dev'
+    )
 
+@pytest.fixture(scope="function",autouse = True )
+def setup_browser(request):
     options = webdriver.ChromeOptions()
     options.browser_version = "100.0"
     # Headless
@@ -23,7 +30,14 @@ def setup_browser():
     browser.config.driver_remote_url = (
         "https://user1:1234@selenoid.autotests.cloud/wd/hub"
     )
-    browser.config.base_url = 'https://test.app.pryaniky.com'
+    envr_value = request.config.getoption('--envr')
+    if envr_value=='dev':
+        envr = '-dev.v5-pre'
+    elif envr_value=='stage':
+        envr = '-rc.v5-stage'
+    elif envr_value == 'prod':
+        envr = '.app'
+    browser.config.base_url = f'https://test{envr}.pryaniky.com'
 
     browser.config.timeout = 30
 
